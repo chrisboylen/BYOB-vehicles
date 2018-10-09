84667837ff6ead3ae5b4151ca5f9ee7e5d589af2
Chris Boylen <billpolster@gmail.com>
2018-10-09 16:30:59 -0600

    Add and pass test so that /api/v1/models endpoint returns all models

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -26,6 +26,33 @@ describe('API ROUTES', () => {
         done();
       });
   });
+
+  it('/api/v1/models should return all makes', (done) => {
+    chai.request(server)
+      .get('/api/v1/models')
+      .end((err, response) => {
+        response.should.have.status(200);
+        response.should.be.json;
+        response.body.should.be.a('array');
+        response.body.length.should.equal(167);
+        response.body[0].should.have.property('model_name');
+        response.body[0].model_name.should.equal('458 Italia');
+        response.body[0].should.have.property('body');
+        response.body[0].body.should.equal('Coupe');
+        response.body[0].should.have.property('engine');
+        response.body[0].engine.should.equal('8');
+        response.body[0].should.have.property('top_speed');
+        response.body[0].should.have.property('horse_power');
+        response.body[0].horse_power.should.equal(578);
+        response.body[0].should.have.property('transmission');
+        response.body[0].transmission.should.equal('7-speed automatic');
+        response.body[0].should.have.property('make_id');
+        response.body[0].created_at.should.equal('2018-10-09T14:35:15.507Z');
+        response.body[0].should.have.property('updated_at');
+        response.body[0].updated_at.should.equal('2018-10-09T14:35:15.507Z');
+        done();
+      });
+  });
 });
 
 describe('CLIENTS ROUTES', () => {
