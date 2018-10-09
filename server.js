50f1ea5a53a397817162986c8a7eb63fa9e8c4ef
Joel O'Connor <joconno07@gmail.com>
2018-10-09 15:00:42 -0600

    Added endpoints to get makes and modles for DB

--- server.js
+++ server.js
@@ -17,6 +17,26 @@ app.get('/', (request, response) => {
   response.send('Server working');
 });
 
+app.get('/api/v1/makes', (request, response) => {
+  database('makes').select()
+    .then((makes) => {
+      response.status(200).json(makes);
+    })
+    .catch((error) => {
+      response.status(500).json({ error });
+    });
+});
+
+app.get('/api/v1/models', (request, response) => {
+  database('models').select()
+    .then((models) => {
+      response.status(200).json(models);
+    })
+    .catch((error) => {
+      response.status(500).json({ error });
+    });
+});
+
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
 });
